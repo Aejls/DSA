e77f5ac2d2d97847a826ee1f3e66094b0c7e8eab
Sejal Chauhan <86917180+Aejls@users.noreply.github.com>
2023-04-22 00:40:25 +0530

    Add files via upload

---
+++ sum from 1 to N.cpp
@@ -0,0 +1,22 @@
+// Print sum from 1 to N
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n;
+	cout<<"Value of n:";
+	cin >>n;
+	
+	int sum=0;
+	
+	for(int i=1; i<=n; i++){
+		sum=sum+i;
+	
+	}
+
+	cout<<sum<<endl;
+
+	
+	
+}
