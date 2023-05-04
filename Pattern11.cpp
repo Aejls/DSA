13955caa95c7c9e12c8dfaf7579e96a0bef50ad9
Sejal Chauhan <86917180+Aejls@users.noreply.github.com>
2023-05-05 02:20:45 +0530

    Add files via upload

---
+++ Pattern11.cpp
@@ -0,0 +1,56 @@
+/*
+1
+21
+321
+4321
+*/
+// Method -1 Using a Variable 
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin>>n;
+	
+	int row=1;
+	while(row<=n){
+		
+		int col=1;
+		int value=row;
+		while(col<=row){
+			
+			cout<<value;
+			value=value-1;
+			col=col+1;
+		}
+		cout<<endl;
+		row =row+1;
+	}
+}
+*/
+// Method-2 Using relation b/w row and column
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin>>n;
+	
+	int row=1;
+	while(row<=n){
+		
+		int col=1;
+	
+		while(col<=row){
+			
+			cout<<row-col+1;
+			
+			col=col+1;
+		}
+		cout<<endl;
+		row =row+1;
+	}
+}
