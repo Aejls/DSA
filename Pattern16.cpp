e53a980dbe13f0811e3c768cc57b5234c6a431ce
Sejal Chauhan <86917180+Aejls@users.noreply.github.com>
2023-05-11 00:41:20 +0530

    Add files via upload

---
+++ Pattern16.cpp
@@ -0,0 +1,26 @@
+/*
+A
+BC
+DEF
+GHIJ 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+    char alphabet = 'A';
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << alphabet;
+            alphabet++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
