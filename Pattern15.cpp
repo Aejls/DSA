633ac1c1abaf80ea310d04e322b33e36a743d86e
Sejal Chauhan <86917180+Aejls@users.noreply.github.com>
2023-05-10 12:12:14 +0530

    Add files via upload

---
+++ Pattern15.cpp
@@ -0,0 +1,38 @@
+/*
+A
+BB
+CCC
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin>>n;
+	
+	
+	int row=1;
+	
+	char ch='A';
+	while(row<=n){
+		
+		int col=1;
+	
+	    
+	   
+		while(col<=row){
+			
+		
+			 
+			cout<<ch <<" ";
+			
+			
+			col=col+1;
+		}
+		ch=ch+1;
+		cout<<endl;
+		row =row+1;
+	}
+	return 0;
+}
